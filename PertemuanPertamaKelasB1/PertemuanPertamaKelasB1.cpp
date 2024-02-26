3c888e38abd8703a61116ae4e588cf033768868b
febry40 <febryevayonisa@gmail.com>
2024-02-26 15:16:23 +0700

    Add project files.

---
+++ PertemuanPertamaKelasB1/PertemuanPertamaKelasB1.cpp
@@ -0,0 +1,29 @@
+// PertemuanPertamaKelasB1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{//begin 
+    // numeric nPanjang, nLebar, nLuas
+    // display 'Masukan Panjang= '
+    // accept nPanjang
+    // display 'Masukan Lebar'
+    // acccept nLebar
+    // compute nLuas = nPanjang + nLebar
+    // display 'Luasnya adalah = ' + nLuas
+
+    int nPanjang, nLebar, nLuas;
+    cout << "Masukan Panjang = ";
+    cin >> nPanjang;
+    cout << "Masukan Lebar = ";
+    cin >> nLebar;
+    nLuas = nPanjang * nLebar;
+    cout << "Luasnya adalah " << nLuas << endl;
+
+    //end 
+
+
+
+      
+    
